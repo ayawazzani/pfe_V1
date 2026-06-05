dda1d7d9c40c7e0b338cfde6498a31ce6553fe26
ayawazzani6-developpeuse <EMAIL_DYALK_F_GITHUB>
2026-06-05 11:23:35 +0100

    Use AlwaysData API

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an Axios instance pointing to the Laravel API
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api',
+    baseURL: 'http://ecomv1.alwaysdata.net/api',
     withCredentials: false,
     headers: {
         Accept: 'application/json',
